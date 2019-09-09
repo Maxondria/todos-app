02f5f8619a3cd7936a2344e0cea6693cf4781bd3
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-09-09 22:52:38 +0300

    [task] Added the Show one todo details feature

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -7,34 +7,26 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           crossorigin="anonymous">
-    <title>Todos List</title>
+    <title>Single Todo</title>
 </head>
 <body>
 <div class="container">
-    <h1 class="text-center my-5">TODOS PAGE</h1>
+    <h1 class="text-center my-5">{{$todo->name}}</h1>
 
     <div class="row justify-content-center">
-
-        <div class="col md-8">
+        <div class="col md-6">
             <div class="card card-default">
                 <div class="card-header">
-                    Todos
+                    Details
                 </div>
 
                 <div class="card-body">
-                    <ul class="list-group">
-                        @foreach($todos as $todo)
-                            <li class="list-group-item">
-                                {{ $todo->name  }}
-                                <a href="/todos/{{$todo->id}}" class="btn btn-primary btn-sm float-right">View</a>
-                            </li>
-                        @endforeach
-                    </ul>
+                    {{$todo->description}}
                 </div>
             </div>
         </div>
-
     </div>
+
 </div>
 
 </body>
